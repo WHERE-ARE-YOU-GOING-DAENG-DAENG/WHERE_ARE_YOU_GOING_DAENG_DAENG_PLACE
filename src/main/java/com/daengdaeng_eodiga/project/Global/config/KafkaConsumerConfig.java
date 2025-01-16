68ea54cd938a456e567ac91fd9008737fbce982d
13wjdgk <13wjdgk@naver.com>
2025-01-16 16:27:40 +0900

    chore : kafka 설정

---
+++ src/main/java/com/daengdaeng_eodiga/project/Global/config/KafkaConsumerConfig.java
@@ -0,0 +1,35 @@
+package com.daengdaeng_eodiga.project.Global.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+	@Bean
+	public ConsumerFactory<String, Object> consumerFactory() {
+		Map<String, Object> config = new HashMap<>();
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:10000");
+		config.put(ConsumerConfig.GROUP_ID_CONFIG, "region_visit_transaction_response");
+		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+		return new DefaultKafkaConsumerFactory<>(config);
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+
+		return factory;
+	}
+}
--- src/main/java/com/daengdaeng_eodiga/project/ProjectApplication.java
+++ src/main/java/com/daengdaeng_eodiga/project/ProjectApplication.java
@@ -2,10 +2,11 @@ package com.daengdaeng_eodiga.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+@EnableDiscoveryClient
 @EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication
